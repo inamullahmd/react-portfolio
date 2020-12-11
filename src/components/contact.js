8e46c6ef1b10a319bb2d514e12daa01efe92eff2
Mohammad Inamullah <31799163+inamullahmdi@users.noreply.github.com>
2020-12-11 21:38:51 +0530

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,14 +6,14 @@ const Contact = () => {
         <Container className="text-center">
             <div id="contact">
                 <h1 className="h1-responsive fw-b my-3">Contact</h1>
-                <h3 className="h3-resonsive fw-sb my-4">Let's create something together</h3>
+                <h4 className="h4-resonsive fw-sb my-4">Let's create something together</h4>
                 <a className="mail-link" href={`mailto:mohammadinamullahmdi@gmail.com`}>
                     mohammadinamullahmdi@gmail.com
                 </a>
                 <div className="social p-4">
                     <FaGithub/> <FaInstagram/> <FaLinkedin/> <FaTwitter/> <FaFacebook/>
                 </div>
-                <h5 class="h5-responsive mb-5">Made with 💝 by <span class="fw-b">Mohammad Inamullah</span> </h5>
+                <h5 class="h5-responsive mb-5">Made with 💝 by <a class="fw-b" href="https://github.com/inamullahmdi">Mohammad Inamullah</a> </h5>
             </div>
         </Container>
     )
